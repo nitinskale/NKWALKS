59bc5074092c141730e13c2636d5223737aa5b04
nsk <nitinskale0712@gmail.com>
2023-01-03 23:21:53 +0530

    GetAllRegions Api

---
+++ NKWalks/NKWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NKWalks.API.Models.Domain;
+
+namespace NKWalks.API.Controllers
+{
+    [ApiController]
+    [Route("API/reg")]
+    public class RegionsController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAllRegions()
+        {
+            var regions = new List<Region>()
+           {
+               new Region
+               {
+                   Id= Guid.NewGuid(),
+                   Name = "Welington",
+                   Code ="WLG",
+                   Area=227755,
+                   Lat=1.8822,
+                   Long=299.88,
+                   Populate=500000
+               },new Region
+               {
+                   Id= Guid.NewGuid(),
+                   Name = "Auckland",
+                   Code ="Auck",
+                   Area=250000,
+                   Lat=2.8822,
+                   Long=29.88,
+                   Populate=600000
+               }
+           };
+            return Ok(regions);
+        }
+    }
+}
