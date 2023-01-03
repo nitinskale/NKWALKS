834ec2338a4de38da303f2d2216406c79d21c552
nsk <nitinskale0712@gmail.com>
2023-01-03 23:05:47 +0530

    End of section two

---
+++ NKWalks/NKWalks.API/Data/NKWalksDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NKWalks.API.Models.Domain;
+
+namespace NKWalks.API.Data
+{
+    public class NKWalksDbContext : DbContext
+    {
+        public NKWalksDbContext(DbContextOptions<NKWalksDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }//if table does not exist then will create table by entity
+
+        public DbSet<Walk> Walks { get; set; }
+
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+    }
+}
---
+++ NKWalks/NKWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,16 @@
+﻿namespace NKWalks.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Area { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+        public long Populate { get; set; }
+
+        //navigation
+        public IEnumerable<Walk> Walks { get; set; }
+    }
+}
